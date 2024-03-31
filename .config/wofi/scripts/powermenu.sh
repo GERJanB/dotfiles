69c50a45a71384d9e82460e563dc09e6e5d5a58d
jbudde <jan.budde@student.ibs-ol.de>
2024-03-31 21:16:38 +0200

    save

--- .config/wofi/scripts/powermenu.sh
+++ .config/wofi/scripts/powermenu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | wofi -i --dmenu | awk '{print tolower($2)}' )
+op=$( echo -e " Poweroff\n Reboot\n Suspend\n Lock\n Logout" | wofi -i --dmenu --width=10% --lines=6 | awk '{print tolower($2)}' )
 
 case $op in 
         poweroff)
