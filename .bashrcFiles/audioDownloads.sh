cd0be3ffea2765cc01f29a5e3aa6cc5f827ebe1f
jbudde <jan.budde@student.ibs-ol.de>
2024-05-19 21:50:11 +0200

    save

--- .bashrcFiles/audioDownloads.sh
+++ .bashrcFiles/audioDownloads.sh
@@ -7,3 +7,14 @@ function findDownloadAudio() {
     echo $url
     yt-dlp --extract-audio --audio-format mp3 --add-metadata -o "./%(title)s.%(ext)s" $url
 }
+
+function removeSpaces() {
+    local input="$1"
+    local escaped="${input// /\\ }"
+    echo "$escaped"
+}
+
+function play() {
+find ~/ -type d -name '.*' -prune -o -type f \( -name "*.mp4" -o -name "*.mkv" -o -name "*.avi" -o -name "*.mov" -o -name "*.mp3" -o -name "*.flac" -o -name "*.wav" \) -print | fzf -q "$*" --preview 'mpv --ao=null --vo=null --quiet {}' --bind 'enter:execute(mpv {})'
+}
+
--- .bashrcFiles/pathExports.sh
+++ .bashrcFiles/pathExports.sh
@@ -7,6 +7,7 @@ export MANPATH=$MANPATH:/usr/local/texlive/2023/texmf-dist/doc/man
 export INFOPATH=$INFOPATH:/usr/local/texlive/2023/texmf-dist/doc/info
 
 export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/home/jbudde/.local/share/flatpak/exports/share:${XDG_DATA_DIRS}"
+export XDG_CONFIG_HOME="/home/jbudde/.config/"
 
 
 export SUDO_EDITOR="nvim"
